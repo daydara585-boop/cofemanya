035c1c8a2ad4f9115bda0f3f50a532927f81b5a1
DayDara <daydara585@gmail.com>
2026-08-04 14:01:23 +0000

    Первая красивая версия Cofeмании

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const button = document.querySelector(".coffee-button");
+
+    button.addEventListener("click", () => {
+
+        alert("☕ Добро пожаловать в Cofeманию.\n\nСкоро здесь откроется уютная кофейня.");
+
+    });
+
+});
